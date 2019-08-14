486928de726842fa80b16d3cb2967e873b9b6d36
rcruz <rcruz@miuandes.cl>
2019-08-14 18:04:30 -0400

    Partiendo ejemplo resta de numeros casi iguales

---
+++ loss-of-significance.py
@@ -0,0 +1,9 @@
+#Se definen dos numeros casi iguales, los cuales al restarlos
+#dan como resulado 0, lo cual es erroneo, dado que no son iguales
+
+a= 2.999999999999999999999999999999999999999999
+b= 2.999999999999999999999999999999999999999998
+
+print float(a-b)
+
+# 
