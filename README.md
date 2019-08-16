757ab01e3972ce8f8ba8848957fa7de3c5c9474a
rcruz <rcruz@miuandes.cl>
2019-08-16 15:14:20 -0400

    peridad de significancia con 15 decimales

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MCOC-Proyecto-0
 Proyecto 0, Metodos Computacionales en IOC
+
+Introducción
+==============
+
+
+
+Este ejemplo
+==============
+
+
+
+
+Resultados
+==============
+
+Se define el error relativo como 
+
+	ERROR = (Promedio_Calculado - Resultado_Exacto) / Resultado_Exacto
--- loss-of-significance.py
+++ loss-of-significance.py
@@ -1,9 +1,32 @@
 #Se definen dos numeros casi iguales, los cuales al restarlos
 #dan como resulado 0, lo cual es erroneo, dado que no son iguales
 
-a= 2.999999999999999999999999999999999999999999
-b= 2.999999999999999999999999999999999999999998
+import scipy as sp
+import matplotlib.pylab as plt
+import sys
 
+
+# Cuando se restan dos numeros parecidos pero no iguales, si 
+# sus decimales son 15, no hay perdida de significancia,  y el
+#programa entregará la solución correctamente, pero si son mas de
+# 15 decimales se probocara perdida de significancia
+
+
+a= 2.999999999999999
+b= 2.999999999999998
+d= 2.9999999999
+c= a-b
+
+print c
 print float(a-b)
+
+lista1=[]
+
+lista1.append(a)
+lista1.append(b)
+lista1.append(c)
+
+print lista1
+print d
 
 # 
